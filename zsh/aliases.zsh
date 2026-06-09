044df648b5f9b3cc93fa92e9a44d237af519e6c4
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-09 12:15:45 +0800

    zsh aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+# projects script path
+export PATH="$HOME/projects/dotfiles/rofi:$PATH"
+
+alias so="source"
+alias c="clear"
+
+# configs
+alias czsh="nvim ~/.config/zsh/aliases.zsh"
+alias csway="nvim ~/.config/sway/config"
+alias ckitty="nvim ~/.config/kitty/kitty.conf"
+
+# reloading
+alias reload-sway="swaymsg reload"
+alias reload-zsh="source ~/.zshrc"
+
+
+# nvim
+alias vim="nvim ."
