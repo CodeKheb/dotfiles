052294b47c7dd80d03f07f2db04a824fc4770fd7
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-10 19:03:41 +0800

    New Project Script

--- rofi/projects.sh
+++ rofi/projects.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-project=$(
-    find "$HOME/projects" -mindepth 1 -maxdepth 1 -type d -printf '%f\n' |
-    rofi -dmenu -i -p "Kheb NVIM" \
-         -theme "$HOME/projects/dotfiles/rofi/project-picker.rasi"
+PROJECTS_DIR="$HOME/projects"
+
+selection=$(
+{
+    echo "New Project"
+    find "$PROJECTS_DIR" -mindepth 1 -maxdepth 1 -type d -printf '%f\n'
+} | rofi -dmenu -i -p "Kheb NVIM" \
+        -theme "$HOME/projects/dotfiles/rofi/project-picker.rasi"
 )
 
-if [[ -n "$project" ]]; then
-    kitty --working-directory "$HOME/projects/$project" \
+[[ -z "$selection" ]] && exit
+
+if [[ "$selection" == "New Project" ]]; then
+    project_name=$(rofi -dmenu -p "Project Name" \
+         -theme "$HOME/projects/dotfiles/rofi/new-project.rasi"
+)
+
+    [[ -z "$project_name" ]] && exit
+
+    mkdir -p "$PROJECTS_DIR/$project_name"
+
+    kitty --working-directory "$PROJECTS_DIR/$project_name" \
+        bash -c "nvim .; exec zsh"
+else
+    kitty --working-directory "$PROJECTS_DIR/$selection" \
         bash -c "nvim .; exec zsh"
 fi
