a54eca1144b47c73a7aeae7489326bedc5d68e6f
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-12 17:32:42 +0800

    Android studio

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,11 @@
-# projects script path
+# path
 export PATH="$HOME/projects/dotfiles/rofi:$PATH"
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export NDK_HOME=$HOME/Android/Sdk/ndk/30.0.14904198
+
+alias sdkmanager="/home/me/Android/Sdk/cmdline-tools/bin/sdkmanager"
 
 # source esp-idf
 alias esp="source ~/esp-idf/export.sh"
@@ -19,5 +25,9 @@ alias reload-zsh="source ~/.zshrc"
 
 # nvim
 alias vim="nvim ."
+
+# java 
+alias java25="sudo archlinux-java set java-25-openjdk"
+alias java21="sudo archlinux-java set java-21-openjdk"
 
 fastfetch
