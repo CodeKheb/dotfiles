e2ba5ad70a6c08e03334379e132cb0ac30291a34
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-25 19:59:27 +0800

    Website Search for rofi

--- website-picker/website-picker.sh
+++ website-picker/website-picker.sh
@@ -13,6 +13,15 @@ declare -A sites=(
     ["Gemini"]="https://gemini.google.com"
 )
 
-choice=$(printf "%s\n" "${!sites[@]}" | sort | rofi -dmenu -i -theme ~/projects/dotfiles/website-picker/website-picker.rasi "Open")
+choice=$(printf "%s\n" "${!sites[@]}" | sort | \
+    rofi -dmenu -i \
+    -p "Search" \
+    -theme ~/projects/dotfiles/website-picker/website-picker.rasi "Open")
 
-[ -n "$choice" ] && firefox "${sites[$choice]}"
+[ -z "$choice"] && exit
+
+if [[ -n "${sites[$choice]}" ]]; then
+    firefox "${sites[$choice]}"
+else
+    firefox "https://www.google.com/search?q=${choice// /+}"
+fi
