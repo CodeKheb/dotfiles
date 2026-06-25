b0515f9d9ac04db818887a3d6d6982a1b442b777
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-25 12:27:41 +0800

    More Websites

--- website-picker/website-picker.sh
+++ website-picker/website-picker.sh
@@ -2,10 +2,15 @@
 
 declare -A sites=(
     ["Facebook"]="https://facebook.com"
-    ["GitHub"]="https://github.com"
-    ["Google Classroom"]="https://classroom.google.com"
+    ["GitHub"]="https://github.com/CodeKheb"
+    ["Classroom"]="https://classroom.google.com"
+    ["Drive"]="https://drive.google.com"
     ["Gmail"]="https://mail.google.com"
     ["YouTube"]="https://youtube.com"
+    ["Canva"]="https://canva.com"
+    ["Claude"]="https://claude.ai"
+    ["ChatGPT"]="https://chatgpt.com"
+    ["Gemini"]="https://gemini.google.com"
 )
 
 choice=$(printf "%s\n" "${!sites[@]}" | sort | rofi -dmenu -i -theme ~/projects/dotfiles/website-picker/website-picker.rasi "Open")
