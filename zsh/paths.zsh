4483a390ea30084fc533af39327528e1a24cccdf
CodeKheb <kherbinbuenaventura@gmail.com>
2026-08-11 19:17:19 +0800

    local

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -3,5 +3,6 @@ export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 export NDK_HOME=$HOME/Android/Sdk/ndk/30.0.14904198
+export PATH="$PATH:$HOME/.local/bin"
 
 
