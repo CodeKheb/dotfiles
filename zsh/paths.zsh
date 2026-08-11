041c8dfe77a8fb3e01b8d6a2f1b84111bbefcbd1
CodeKheb <kherbinbuenaventura@gmail.com>
2026-08-11 23:20:15 +0800

    Go

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,8 +1,16 @@
-export PATH="$HOME/projects/dotfiles/rofi:$PATH"
+# ndk & sdk
+export NDK_HOME=$HOME/Android/Sdk/ndk/30.0.14904198
 export ANDROID_HOME=$HOME/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-export NDK_HOME=$HOME/Android/Sdk/ndk/30.0.14904198
+
+# rofi
+export PATH="$HOME/projects/dotfiles/rofi:$PATH"
+
+# bin
 export PATH="$PATH:$HOME/.local/bin"
+
+# go
+export PATH=$PATH:$(go env GOPATH)/bin
 
 
