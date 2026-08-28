ea5e0d9a7f74a5fa15fc9a9c1210eb7370cac7c3
CodeKheb <kherbinbuenaventura@gmail.com>
2026-08-28 18:11:59 +0800

    Telescope ignores binary files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,14 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+require('telescope').setup({
+  defaults = {
+    file_ignore_patterns = {
+      "%.png", "%.jpg", "%.jpeg", "%.gif", "%.webp",
+      "%.pdf", "%.zip", "%.tar", "%.gz", "%.rar",
+      "%.exe", "%.dll", "%.so", "%.o", "%.a",
+      "%.bin", "%.mp4", "%.mp3", "%.wav"
+    },
+  },
+})
