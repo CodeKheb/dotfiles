36ed2f3d47b9ba31745750c41ef7af90999b1bf9
CodeKheb <kherbinbuenaventura@gmail.com>
2026-08-30 03:18:18 +0800

    Script loads with tmux sessions now

--- rofi/projects.sh
+++ rofi/projects.sh
@@ -22,8 +22,8 @@ if [[ "$selection" == "New Project" ]]; then
     mkdir -p "$PROJECTS_DIR/$project_name"
 
     kitty --working-directory "$PROJECTS_DIR/$project_name" \
-        bash -c "nvim .; exec zsh"
+        bash -c "nvim .; exec tmux new-session \; new-window \; new-window \; new-window"
 else
     kitty --working-directory "$PROJECTS_DIR/$selection" \
-        bash -c "nvim .; exec zsh"
+        bash -c "nvim .; exec tmux new-session \; new-window \; new-window \; new-window"
 fi
