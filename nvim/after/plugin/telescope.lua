538872f68d38a29dd3d360bdeb913999fe0eafec
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-03 22:01:47 +0800

    telescope can see where y'all are hiding

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true }) end, {})
+vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ hidden = true }) end, {})
+vim.keymap.set("n", "<leader>fb", function() builtin.buffers({ hidden = true }) end, {})
+vim.keymap.set("n", "<leader>fh", function() builtin.help_tags({ hidden = true }) end, {})
 
 require('telescope').setup({
   defaults = {
