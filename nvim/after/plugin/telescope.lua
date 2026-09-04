9559269a41ff7041e9b42cbe99b4e8584fb36f1d
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-04 22:17:27 +0800

    Don't show hidden idk

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set("n", "<leader>ff", function() builtin.find_files({ hidden = true }) end, {})
-vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({ hidden = true }) end, {})
+vim.keymap.set("n", "<leader>ff", function() builtin.find_files({}) end, {})
+vim.keymap.set("n", "<leader>fg", function() builtin.live_grep({}) end, {})
 vim.keymap.set("n", "<leader>fb", function() builtin.buffers({ hidden = true }) end, {})
 vim.keymap.set("n", "<leader>fh", function() builtin.help_tags({ hidden = true }) end, {})
 
