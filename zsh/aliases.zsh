3f1481afed3894320f8c329f5ca3c76a93e54924
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-19 23:11:16 +0800

    monitor

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -20,7 +20,8 @@ alias reload-zsh="source ~/.zshrc"
 
 alias arduino="/opt/arduino/arduino-ide > /dev/null 2>&1 &"
 alias arduino-8266-compile="arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 -u -p /dev/ttyUSB0"
-alias arudino-8266-monitor="arduino-cli monitor -p /dev/ttyUSB0 -b esp8266:esp8266:nodemcuv2"
+alias arduino-8266-check="arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2"
+alias arduino-8266-monitor="arduino-cli monitor -p /dev/ttyUSB0 -b esp8266:esp8266:nodemcuv2"
 
 
 # nvim
