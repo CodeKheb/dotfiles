093f93c93e95f338eb8ce3dddb94aab057ed338f
CodeKheb <kherbinbuenaventura@gmail.com>
2026-09-22 13:27:21 +0800

    clean this

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,18 +1,16 @@
 # ndk & sdk
-export NDK_HOME=$HOME/Android/Sdk/ndk/30.0.14904198
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+export NDK_HOME="$HOME/Android/Sdk/ndk/30.0.14904198"
+export ANDROID_HOME="$HOME/Android/Sdk"
 
-# rofi
+# tools
+export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/projects/dotfiles/rofi:$PATH"
 
-# bin
-export PATH="$PATH:$HOME/.local/bin"
-
 # go
-export PATH="$PATH:/usr/local/go/bin"
-export PATH=$PATH:$HOME/go/bin
+export PATH="/usr/local/go/bin:$HOME/go/bin:$PATH"
 
-# dotnet 
-export PATH="$PATH:$HOME/.dotnet/tools"
+# android
+export PATH="$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$PATH"
+
+# dotnet
+export PATH="$HOME/.dotnet/tools:$PATH"
